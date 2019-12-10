d673e1d49603166e57a59c60908e4abf1301d7f0
kael <cdcp-1795@hotmail.com>
2019-12-10 22:19:12 +0000

    a8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for pos in range(0, len(str)):
         code = ord(str[pos])
-        if code > 96 and code < 122:
+        if code > 96 and code < 123:
             code = code - 32
         print("{:s}".format(chr(code)), end='')
     print("{:s}".format(''))
