4c59584b6d9823dd902f806c8fabcedd31e4889c
kael <cdcp-1795@hotmail.com>
2019-12-10 20:34:54 +0000

    a8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        code = ord(letter)
+        if code > 97 and code < 122:
+            code = code - 32
+            print(chr(code) + "")
+        else:
+            print(letter + "")
+uppercase("holberton 98")
