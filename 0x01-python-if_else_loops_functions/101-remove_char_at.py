fddb4a63ffdd10d3e256168006acd06a263a28e2
kael <cdcp-1795@hotmail.com>
2019-12-11 02:34:36 +0000

    a15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,9 +4,3 @@ def remove_char_at(str, n):
     if n >= 0 and len(word) > n:
         del word[n]
     return("".join(word))
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
