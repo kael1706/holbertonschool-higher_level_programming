f65f04784c7aa8e73d1621721ef5ee93c0655c83
kael <cdcp-1795@hotmail.com>
2019-12-12 17:10:04 +0000

    fix a0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     a = 1
     b = 2
 
-    print(str(a) + " + " + str(b) + " = " + str(add(a, b)))
+#    print(str(a) + " + " + str(b) + " = " + str(add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
