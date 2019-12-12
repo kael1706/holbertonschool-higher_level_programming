44e5c3ef8219a81c506eff73b109a42a4394df9e
kael <cdcp-1795@hotmail.com>
2019-12-12 15:29:49 +0000

    a2

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+print (str(a) + " + " + str(b) + " = " + str(add(a, b)))
+print (str(a) + " - " + str(b) + " = " + str(sub(a, b)))
+print (str(a) + " * " + str(b) + " = " + str(mul(a, b)))
+print (str(a) + " / " + str(b) + " = " + str(div(a, b)))
