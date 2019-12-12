0c8e9b3794282cb15e97d45a53abcdb2a41e6d85
kael <cdcp-1795@hotmail.com>
2019-12-12 18:12:27 +0000

    a0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ from add_0 import add
 if __name__ == '__main__':
     a = 1
     b = 2
- #eeeee
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print(str(a) + " + " + str(b) + " = " + str(add(a, b)))
