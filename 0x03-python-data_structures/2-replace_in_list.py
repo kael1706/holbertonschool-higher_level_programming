d845e32549b30ff1ca24863621be659abcbe0564
kael <cdcp-1795@hotmail.com>
2019-12-17 22:36:09 +0000

    fix 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
         return my_list
     for n_id, v in enumerate(my_list):
         if n_id == idx:
-            my_list[n_id] == element
+            my_list[n_id] = element
             return my_list
 
 """my_list = [1, 2, 3, 4, 5]
