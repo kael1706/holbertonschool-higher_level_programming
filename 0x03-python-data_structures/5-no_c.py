79b9710122667cb4fdb01c41e6d0d0e997cf3931
kael <cdcp-1795@hotmail.com>
2019-12-17 23:29:24 +0000

    a5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    a = list(my_string)
+    for n_id, v in enumerate(a):
+        if v == 'C' or v == 'c':
+            del a[n_id]
+    return "".join(a)
+"""print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
+"""
