21cadae048f8428defe36ca07126a9597e914270
kael <cdcp-1795@hotmail.com>
2019-12-18 02:26:21 +0000

    a8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if sentence == None:
+        return (0, None)
+    return len(sentence), sentence[0]
+
+"""sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+"""
