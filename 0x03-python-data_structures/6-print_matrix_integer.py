9e14800d83f0cb4910dc8944941a413a97bd9ff5
kael <cdcp-1795@hotmail.com>
2019-12-18 01:20:15 +0000

    fix a6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for r, vr in enumerate(matrix):
-        for c, vc in enumerate(vr):
-            print("{:d}".format(matrix[r][c]), end=" ")
-        print("{:s}".format(''))
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            print("{:d}".format(matrix[r][c]), end=' ')
+        print("")
+
 """matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
 print_matrix_integer(matrix)
 print("--")
