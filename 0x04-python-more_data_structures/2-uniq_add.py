831598ef62a8d32e4f49d122a9b4f98d284b84c2
kael <cdcp-1795@hotmail.com>
2019-12-19 22:33:52 +0000

    a2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    my_list = list(set(my_list))
+    c = 0
+    for item in my_list:
+        c = c + item
+    return c
+"""
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
+print("Result: {:d}".format(result))
+"""
