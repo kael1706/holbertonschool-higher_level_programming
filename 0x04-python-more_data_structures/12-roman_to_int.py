1cfbb8a270bcbd6b2da4ef1b694c216135428ee5
kael <cdcp-1795@hotmail.com>
2019-12-20 04:14:37 +0000

    a12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string == None:
+    if type(roman_string) is not str or roman_string is None:
         return(0)
     answer = 0
     table = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
