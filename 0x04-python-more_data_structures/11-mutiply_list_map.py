e185adfac50f13b66905486c0dac4df2545b9fad
kael <cdcp-1795@hotmail.com>
2019-12-20 02:19:29 +0000

    a11

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def mutiply_list_map(my_list=[], number=0):
+    return(list(map(lambda e: e * number, my_list)))
+"""
+my_list = [1, 2, 3, 4, 6]
+new_list = mutiply_list_map(my_list, 4)
+print(new_list)
+print(my_list)"""
