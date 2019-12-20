7dd61ce7793a9a981fcbec93cc865c070f352135
kael <cdcp-1795@hotmail.com>
2019-12-20 00:25:10 +0000

    fix a7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,9 @@ def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
 """
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+nf = '6-print_sorted_dictionary'
+print_sorted_dictionary = __import__(nf).print_sorted_dictionary
+a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
 print_sorted_dictionary(new_dict)
 print("--")
