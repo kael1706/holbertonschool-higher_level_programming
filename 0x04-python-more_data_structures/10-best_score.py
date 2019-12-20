43a095fa0df02bbfb16dd8065db8d6c992d6c781
kael <cdcp-1795@hotmail.com>
2019-12-20 02:11:22 +0000

    a10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        c = 0
+        w = ''
+        for k, v in a_dictionary.items():
+            if v > c:
+                c = v
+                w = k
+        return w
+    return None
+"""
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
+"""
