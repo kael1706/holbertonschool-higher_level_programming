00c9bcf42694170e348399d132515d62fde630f8
kael1706 <cdcp-1795@hotmail.com>
2020-01-09 17:42:46 -0500

    a6

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -6,7 +6,7 @@ my_square_1.my_print()
 
 print("--")
 
-my_square_2 = Square(3, (1, "d"))
+my_square_2 = Square(3, (1, 1))
 my_square_2.my_print()
 
 print("--")
@@ -15,8 +15,35 @@ my_square_3 = Square(3, (3, 0))
 my_square_3.my_print()
 
 print("--")
+"""
 
-my_square_4 = Square(-3, (-1, 0))
+my_square_4 = Square(-3)
 my_square_4.my_print()
 
 print("--")
+
+my_square_4 = Square(4, (-1, 0))
+my_square_4.my_print()
+
+print("--")
+
+my_square_4 = Square(5, (0, -1))
+my_square_4.my_print()
+
+print("--")
+
+my_square_4 = Square(6, ("a", 0))
+my_square_4.my_print()
+
+print("--")
+
+my_square_4 = Square(7, (0, "e"))
+my_square_4.my_print()
+
+print("--")
+
+my_square_4 = Square(3, ("a", "b"))
+my_square_4.my_print()
+
+print("--")
+"""
