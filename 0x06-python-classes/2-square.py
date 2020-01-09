987d6e9772929aeb07db4a4d24d209dccb65de78
kael1706 <cdcp-1795@hotmail.com>
2020-01-09 11:49:34 -0500

    a3 and fix a2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,10 +3,8 @@ class Square:
     """."""
     def __init__(self, size=0):
         """start of class"""
-        try:
-            self.__size = size
-            if isinstance(size, int) == False:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
