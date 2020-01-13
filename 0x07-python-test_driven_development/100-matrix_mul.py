26ff6fe580c4d4ee89f2b122e927f5d567e3984c
kael1706 <cdcp-1795@hotmail.com>
2020-01-13 13:32:06 -0500

    adv6 advancement

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+this is the module 100-matrix_mul.
+this module have the following functions:
+matrix_mul(m_a, m_b): multiplies 2 matrices.
+"""
+
+
+def matrix_mul(m_a, m_b):
+    if isinstance(m_a, list) is False:
+        raise TypeError("m_a must be a list")
+    if isinstance(m_b, list) is False:
+        raise TypeError("m_b must be a list")
+    if len(m_a) is 0 or m_a is None:
+        raise TypeError("m_a can't be empty")
+    if len(m_b) is 0 or m_b is None:
+        raise TypeError("m_b can't be empty")
+    m_a_row_size = 0
+    only_one_time = False
+    m_a_rows_count = 0
+    for r in m_a:
+        m_a_rows_count = m_a_rows_count + 1
+        if isinstance(r, list) is False:
+            raise TypeError("m_a must be a list of lists")
+        if len(r) is 0 or r is None:
+            raise TypeError("m_a can't be empty")
+        if only_one_time is False:
+            m_a_row_size = len(r)
+            only_one_time = True
+        tmp = len(r)
+        if m_a_row_size != tmp:
+            raise TypeError("each row of m_a must be of the same size")
+        for c in r:
+            if isinstance(c, (int, float)) is False:
+                raise TypeError("m_a should contain only integers or floats")
+    only_one_time = False
+    m_b_row_size = 0
+    m_b_rows_count = 0
+    for r in m_b:
+        m_b_rows_count = m_b_rows_count + 1
+        if isinstance(r, list) is False:
+            raise TypeError("m_b must be a list of lists")
+        if len(r) is 0 or r is None:
+            raise TypeError("m_b can't be empty")
+        if only_one_time is False:
+            m_b_row_size = len(r)
+            only_one_time = True
+        tmp = len(r)
+        if m_b_row_size != tmp:
+            raise TypeError("each row of m_a must be of the same size")
+        for c in r:
+            if isinstance(c, (int, float)) is False:
+                raise TypeError("m_b should contain only integers or floats")
+    print(m_a)
+    print(m_b)
+    print("m_a_c: " + str(m_a_row_size))
+    print("m_b_r: " + str(m_b_rows_count))
+    if m_a_row_size is m_b_rows_count:
+        return True
+    else:
+        raise TypeError("m_a and m_b can't be multiplied")
