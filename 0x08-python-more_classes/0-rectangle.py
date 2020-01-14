e7510f7006a781ca24fe5154033305d8c465ed9b
kael1706 <cdcp-1795@hotmail.com>
2020-01-14 16:09:28 -0500

    a0, a1, a2

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+this is the class Rectangle
+"""
+
+
+class Rectangle():
+    """."""
+    def __init__(self):
+        pass
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,53 @@
 #!/usr/bin/python3
+"""
+this is the class Rectangle.
+"""
+
+
+class Rectangle():
+    """."""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """
+        get width.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        set width.
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        get height.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        set height.
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
