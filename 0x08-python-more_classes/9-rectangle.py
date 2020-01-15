5461b2525156a99797ca8171b9753e873201962f
kael <cdcp-1795@hotmail.com>
2020-01-15 05:35:53 +0000

    try fix a9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -102,10 +102,10 @@ class Rectangle():
             raise TypeError("rect_1 must be an instance of Rectangle")
         if isinstance(rect_2, Rectangle) is False:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() < rect_2.area():
-            return rect_2
         if rect_1.area() >= rect_2.area():
             return rect_1
+        if rect_1.area() < rect_2.area():
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
