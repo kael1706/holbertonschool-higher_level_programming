ff61135394783c29916ae92e7a8fbd4baae44f4c
kael <cdcp-1795@hotmail.com>
2020-01-24 22:52:51 +0000

    a1...a5

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+this is the module base.
+this module has the followings classes:
+base.
+"""
+
+
+class Base():
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialize attributes"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+this is the module rectangule.
+this module has the following classes:
+Rectangle.
+"""
+
+from models.base import Base
+
+
+def int_check(n, v):
+    """check if is a valid integer."""
+    w = ['width', 'height', 'x', 'y']
+    if type(v) != int:
+        raise TypeError("{} must be an integer".format(n))
+    if (n == w[0] or n == [1]) and v <= 0:
+        raise ValueError("{} must be > 0".format(n))
+    if (n == w[2] or n == w[3]) and v < 0:
+        raise ValueError("{} must be >= 0".format(n))
+
+
+class Rectangle(Base):
+    """Rectangle class extends from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize the attributtes."""
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def area(self):
+        """return the area of the instance."""
+        return self.__width * self.__height
+
+    def display(self):
+        for r in range(self.__height):
+            print('#' * self.__width)
+
+    @property
+    def width(self):
+        """get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width."""
+        int_check('width', value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height."""
+        int_check('height', value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """get x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """set x."""
+        int_check('x', value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """get y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """set y."""
+        int_check('y', value)
+        self.__y = value
