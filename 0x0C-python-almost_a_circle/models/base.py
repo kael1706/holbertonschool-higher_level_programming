43ef2c43fac97818bc9381d73fb2420bf58b7c34
kael <cdcp-1795@hotmail.com>
2020-01-28 01:55:56 +0000

    adv20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,6 +6,7 @@ base.
 """
 import json
 import os
+import csv
 
 
 class Base():
@@ -77,3 +78,32 @@ class Base():
         for d in l_d:
             l_i.append(cls.create(**d))
         return l_i
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """CSV format"""
+        l_o = []
+        n = cls.__name__ + '.csv'
+        if not list_objs or list_objs is None:
+            l_o = []
+        else:
+            l_o = [item.to_dictionary() for item in list_objs]
+        with open(n, 'w+', encoding='utf-8') as f:
+            w = csv.writer(
+                f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            w.writerow([l_o])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """(CSV format)^-1"""
+        l_d_serialized = []
+        l_d_deserialized = []
+        n = cls.__name__ + '.csv'
+        if not os.path.exists(n):
+            return []
+        with open(n, 'r', encoding='utf-8') as f:
+            l_d_serialized = csv.DictReader(f.read())
+        for d in l_d_serialized:
+            l_d_deserialized.append(cls.create(**d))
+        return l_d_deserialized
