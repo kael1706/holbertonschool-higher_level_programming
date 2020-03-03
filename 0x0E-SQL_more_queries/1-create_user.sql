4a7efdf54dcb12da46a67e2c45f83a9a5b89cf60
kael1706 <cdcp-1795@hotmail.com>
2020-03-03 23:02:00 +0000

    a1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- creates the MySQL server user user_0d_1 with all privileges.
+-- cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+-- see changes:
+-- cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
