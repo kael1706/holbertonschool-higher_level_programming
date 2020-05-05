db7f62b5368ea91ae75b0f2c497c20f94143dccb
kael <cdcp-1795@hotmail.com>
2020-05-05 18:04:35 +0000

    the checker does not support the other way

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-// show 3 msg but with loop
+/* show 3 msg but with loop
+ msgs.forEach(item => { console.log(item); });
+*/
 const msgs = [
   'C is fun',
   'Python is cool',
   'Javascript is amazing'
 ];
-msgs.forEach(item => { console.log(item); });
+for (let i = 0; i < msgs.length; i++) {
+  console.log(msgs[i]);
+}
