caf4347a52a5ebed8ca55925935a24543e831944
kael <cdcp-1795@hotmail.com>
2020-05-12 23:43:43 +0000

    [Fix] task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 // prints the title of a Star Wars movie where the episode number matches a given integer.
 const request = require('request');
-request('https://swapi.co/api/films/' + process.argv[2], (e, r, d) => {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, (e, r, d) => {
   if (e) {
     console.log(e);
   } else {
