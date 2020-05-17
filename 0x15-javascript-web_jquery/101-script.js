6da53e47d3fdfb29b74ae92f4d03b505fc36b352
kael <cdcp-1795@hotmail.com>
2020-05-17 19:03:22 +0000

    [fix] now it works in any circumstance

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,9 +1,11 @@
-$('div#add_item').click(function () {
-  $('ul.my_list').append('<li>Item</li>');
-});
-$('div#remove_item').click(function () {
-  $('ul.my_list li')[0].remove();
-});
-$('div#clear_list').click(function () {
-  $('ul.my_list li').remove();
+$(document).ready(function () {
+  $('div#add_item').click(function () {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+  $('div#remove_item').click(function () {
+    $('ul.my_list li')[0].remove();
+  });
+  $('div#clear_list').click(function () {
+    $('ul.my_list li').remove();
+  });
 });
